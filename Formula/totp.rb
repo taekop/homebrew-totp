54af8d226db7887e565371c4ec9dd8d61e7a8a6a
taekop <taekop@naver.com>
2022-03-17 14:20:31 +0900

    v1.1

--- Formula/totp.rb
+++ Formula/totp.rb
@@ -1,16 +1,16 @@
 class Totp < Formula
     desc "TOTP CLI"
     homepage "https://github.com/taekop/homebrew-totp"
-    version "1.0"
+    version "1.1"
 
-    url "https://github.com/taekop/homebrew-totp/releases/download/v1.0/totp-1.0.tar.gz"
-    sha256 "a64b45653a3bc692d46159db3ddcb73fc45c1f63656b6e50ece2edf0e7868275"
+    url "https://github.com/taekop/homebrew-totp/releases/download/v1.1/totp-1.1.tar.gz"
+    sha256 "aa288160545077f8e14a319d79b02dc1f01560cc724838e6f436039633de5787"
 
     def install
         bin.install "bin/execute" => "totp"
-        libexec.install "libexec/main"
-        libexec.install "libexec/totp"
-        libexec.install "libexec/yaml_config"
-        libexec.install "libexec/pretty_print"
+        libexec.install "libexec/main.rb"
+        libexec.install "libexec/totp.rb"
+        libexec.install "libexec/yaml_config.rb"
+        libexec.install "libexec/pretty_print.rb"
     end
 end
